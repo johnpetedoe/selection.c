02feb400f2a4739557e61289413f86e910753e5f
johnpetedoe <johnpetedoe@proton.me>
2026-08-04 23:35:09 +0000

    update

--- main.c
+++ main.c
@@ -1,52 +1,134 @@
 #include <unistd.h>
 
-
-int main ( void ) {
-    t_position *P;
-    t_position *nth_sub_posotion_P;
-
-    P = new_position();                                           // P
-    n_th_sub_position_P = P->morphisms[1](P);                     // (p ..| P)
-    
+void f0 (t_position *P, t_position  *nth_sub_position_P) {
     assert( 
         is_vector(
-            nth_sub_position_P->morphisms[0](P)
+            position->
+                morphisms->
+                    implementations[0][0](n_th_sub_position_P, P);
         ) 
     == true );                                                    // (INCL ((p .. | P) P) V)
 
     assert( 
         is_vector( 
-            nth_sub_position_P->morphisms[2](                    
-                sub_position_P
+            position->
+                morphisms->
+                    implementations[2][0](                    
+                n_th_sub_position_P
             ) 
         ) 
     == true );                                                   // ( INCL (m (p .. | P)) V)
 
     assert(                                                     
         is_real_number( 
-            nth_sub_position_P->properties[0]( 
-                nth_sub_position_P 
-            ) 
+            nth_sub_position_P->
+                properties[0]( 
+                    nth_sub_position_P 
+                ) 
         ), 
         == true 
     );                                                           // (INCL (E (p .. | P)) R)       # D                                                                 
     
     assert( 
         is_real_number(
-            P->properties[0]( P ) 
+            P->properties[0]( P ); 
         ),
         == true;
-    );                                                          // (INCL (E P) )
+    );                                                          // (INCL (E P) R) 
+    return (void);
+}
 
-    /////
-
-    assert(
+void f1 (t_categorey *position, t_position *P, t_position *n_th_sub_position_P) {
+    assert (
         is_vector (
-            subtract(nth_sub_position_P, P);
+            position->
+                subtract(
+                    nth_sub_position_P, 
+                    P
+                );
         ),
-        == true;                                                 // (- (p ..|P) P)
+        == true;                                                // (INCL (- (p ..|P) P) V)
+    );
+
+    assert (
+        is_vector(
+            position->subtract(
+                n_th_sub_position_P,
+                compliment(P)
+            )
+        )
+        == true
+    );                                                            // (- (p..|P) {~P})     
+
+    assert (
+        is_set(
+            position->
+                morphisms->
+                    implementations[4][0](
+                        0, 
+                        position->
+                            morphisms->
+                                length
+                        
+                    )
+        )
+        == true;                                //(INCL {0 .. (length morphisms)} S)
     )
 
+    assert (
+        is_natural_number (
+            resolve(
+                position->
+                    morphisms->
+                        implementations[4][0](
+                            0, 
+                            position->
+                                morphisms->
+                                    length
+                        
+                        )
+            )
+        )
+        == true;
+    )
 
+    assert (
+        position->morphisms[3][0](
+            compliment(P)->properties[
+                position->
+                    morphisms->
+                        resolve(
+                            position->
+                                morphisms->
+                                    implementations[4][0](
+                                        0, 
+                                        position->
+                                            morphisms->
+                                                length
+                                        
+                                    )
+                        )
+            ]
+        );
+    );
+    
+    assert (
+        
+    );
+
+    return (0);
+}
+
+int main ( void ) {
+    t_categorey     position;
+    t_position      *P;
+    t_position      *nth_sub_posotion_P;
+
+    P = new_position();                                           // P
+    n_th_sub_position_P = P->morphisms[1][0](P);                     // (p ..| P)
+    
+    assert(
+
+    );
     return (0);
 }
